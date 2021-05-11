63b20e0d22145bf01e1dbc198964c19aeb16351f
Lukasz <pietraszek@hotmail.co.uk>
2021-05-11 21:22:34 +0200

    backend

---
+++ backend/.keystone/admin/pages/_app.js
@@ -0,0 +1,207 @@
+import React from 'react';
+
+import { KeystoneProvider } from '@keystone-next/admin-ui/context';
+import { ErrorBoundary } from '@keystone-next/admin-ui/components';
+import { Core } from '@keystone-ui/core';
+
+import * as view613b5443 from '../../../node_modules/@keystone-next/fields/types/mongoId/views';
+import * as view11c5aa72 from '../../../node_modules/@keystone-next/fields/types/text/views';
+import * as view2b1ee0e7 from '../../../node_modules/@keystone-next/fields/types/password/views';
+import * as viewee3e43b4 from '../../../node_modules/@keystone-next/fields/types/timestamp/views';
+import * as viewbe9dd177 from '../../../node_modules/@keystone-next/fields/types/select/views';
+import * as viewf7326fcb from '../../../node_modules/@keystone-next/fields/types/integer/views';
+
+const adminConfig = {};
+
+const fieldViews = {
+  view613b5443,
+  view11c5aa72,
+  view2b1ee0e7,
+  viewee3e43b4,
+  viewbe9dd177,
+  viewf7326fcb,
+};
+
+const lazyMetadataQuery = {
+  kind: 'Document',
+  definitions: [
+    {
+      kind: 'OperationDefinition',
+      operation: 'query',
+      selectionSet: {
+        kind: 'SelectionSet',
+        selections: [
+          {
+            kind: 'Field',
+            name: {
+              kind: 'Name',
+              value: 'keystone',
+              loc: { start: 22, end: 30 },
+            },
+            arguments: [],
+            directives: [],
+            selectionSet: {
+              kind: 'SelectionSet',
+              selections: [
+                {
+                  kind: 'Field',
+                  name: {
+                    kind: 'Name',
+                    value: 'adminMeta',
+                    loc: { start: 39, end: 48 },
+                  },
+                  arguments: [],
+                  directives: [],
+                  selectionSet: {
+                    kind: 'SelectionSet',
+                    selections: [
+                      {
+                        kind: 'Field',
+                        name: {
+                          kind: 'Name',
+                          value: 'lists',
+                          loc: { start: 59, end: 64 },
+                        },
+                        arguments: [],
+                        directives: [],
+                        selectionSet: {
+                          kind: 'SelectionSet',
+                          selections: [
+                            {
+                              kind: 'Field',
+                              name: {
+                                kind: 'Name',
+                                value: 'key',
+                                loc: { start: 77, end: 80 },
+                              },
+                              arguments: [],
+                              directives: [],
+                              loc: { start: 77, end: 80 },
+                            },
+                            {
+                              kind: 'Field',
+                              name: {
+                                kind: 'Name',
+                                value: 'isHidden',
+                                loc: { start: 91, end: 99 },
+                              },
+                              arguments: [],
+                              directives: [],
+                              loc: { start: 91, end: 99 },
+                            },
+                            {
+                              kind: 'Field',
+                              name: {
+                                kind: 'Name',
+                                value: 'fields',
+                                loc: { start: 110, end: 116 },
+                              },
+                              arguments: [],
+                              directives: [],
+                              selectionSet: {
+                                kind: 'SelectionSet',
+                                selections: [
+                                  {
+                                    kind: 'Field',
+                                    name: {
+                                      kind: 'Name',
+                                      value: 'path',
+                                      loc: { start: 131, end: 135 },
+                                    },
+                                    arguments: [],
+                                    directives: [],
+                                    loc: { start: 131, end: 135 },
+                                  },
+                                  {
+                                    kind: 'Field',
+                                    name: {
+                                      kind: 'Name',
+                                      value: 'createView',
+                                      loc: { start: 148, end: 158 },
+                                    },
+                                    arguments: [],
+                                    directives: [],
+                                    selectionSet: {
+                                      kind: 'SelectionSet',
+                                      selections: [
+                                        {
+                                          kind: 'Field',
+                                          name: {
+                                            kind: 'Name',
+                                            value: 'fieldMode',
+                                            loc: { start: 175, end: 184 },
+                                          },
+                                          arguments: [],
+                                          directives: [],
+                                          loc: { start: 175, end: 184 },
+                                        },
+                                      ],
+                                      loc: { start: 159, end: 198 },
+                                    },
+                                    loc: { start: 148, end: 198 },
+                                  },
+                                ],
+                                loc: { start: 117, end: 210 },
+                              },
+                              loc: { start: 110, end: 210 },
+                            },
+                          ],
+                          loc: { start: 65, end: 220 },
+                        },
+                        loc: { start: 59, end: 220 },
+                      },
+                    ],
+                    loc: { start: 49, end: 228 },
+                  },
+                  loc: { start: 39, end: 228 },
+                },
+              ],
+              loc: { start: 31, end: 234 },
+            },
+            loc: { start: 22, end: 234 },
+          },
+          {
+            kind: 'Field',
+            name: { kind: 'Name', value: 'authenticatedItem' },
+            selectionSet: {
+              kind: 'SelectionSet',
+              selections: [
+                {
+                  kind: 'InlineFragment',
+                  typeCondition: {
+                    kind: 'NamedType',
+                    name: { kind: 'Name', value: 'User' },
+                  },
+                  selectionSet: {
+                    kind: 'SelectionSet',
+                    selections: [
+                      { kind: 'Field', name: { kind: 'Name', value: 'id' } },
+                      { kind: 'Field', name: { kind: 'Name', value: 'name' } },
+                    ],
+                  },
+                },
+              ],
+            },
+          },
+        ],
+      },
+    },
+  ],
+};
+
+export default function App({ Component, pageProps }) {
+  return (
+    <Core>
+      <KeystoneProvider
+        adminConfig={adminConfig}
+        adminMetaHash="fnpro"
+        fieldViews={fieldViews}
+        lazyMetadataQuery={lazyMetadataQuery}
+      >
+        <ErrorBoundary>
+          <Component {...pageProps} />
+        </ErrorBoundary>
+      </KeystoneProvider>
+    </Core>
+  );
+}
